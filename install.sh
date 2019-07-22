c7cc8f863a67f101c56b01f4cd74342938a996de
Mario Alexis <mario.akexis88@gmail.com>
2019-07-22 14:49:28 -0400

    Add dotfiles installer

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+
+
+function confirm()
+{
+    read -p "$1 [y/N]" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        return 0
+    fi
+    return 1
+}
+
+function link_to_home()
+{
+    for f in ${1}; do
+        if [ ! -s $DOTFILES/$f ]; then
+            echo "$f not found downloading"
+            curl -sL -o $DOTFILES/$f https://raw.githubusercontent.com/MarioAlexis/dotfile/master/$f
+        fi
+
+        if [ -h $HOME/$f ]; then
+            if [ $(readlink $HOME/$f) = $DOTFILES/$f ]; then
+                echo $f already installed
+            fi
+        else
+            if [ -f $HOME/$f ]; then
+                if confirm "Overwrite $HOME/$f?"; then
+                    rm -rf $HOME/$f
+                    ln -sf $DOTFILES/$f $HOME/$f
+                    echo $HOME/$f installed!
+                fi
+            else
+                ln -sf $DOTFILES/$f $HOME/$f
+                echo $HOME/$f installed!
+            fi
+        fi
+    done
+}
+    
+##################
+#   MAIN
+##################
+
+DOTFILES_TO_INSTALL=".gitconfig .tmux.conf .vimrc .zshrc .bashrc .shell_alias .shell_env .shell_functions .ctags"
+link_to_home "$DOTFILES_TO_INSTALL"
